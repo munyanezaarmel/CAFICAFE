febc8d281c6a368d8efa99d861e337266756ed91
Ian Karanja <ian.karanja@strathmore.edu>
2025-06-30 18:42:21 +0300

    Fixed bugs

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,16 +29,16 @@ app = FastAPI(
 # -------------------------------------------------------------------
 # CORS Configuration (NO custom middleware)
 # -------------------------------------------------------------------
-origins = [
-    "https://munyanezaarmel.github.io",  # ✅ GitHub Pages domain
-    "http://localhost:3000",
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-]
+# origins = [
+#     "https://munyanezaarmel.github.io",  # ✅ GitHub Pages domain
+#     "http://localhost:3000",
+#     "http://127.0.0.1:5500",
+#     "http://localhost:5500",
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
